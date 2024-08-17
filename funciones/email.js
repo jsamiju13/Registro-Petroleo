34faafe994c77f452fc9f41d00a26b47a24d157f
Samuel Rivero <jsamiju13@gmail.com>
2024-08-17 03:31:30 -0400

    Update email.js

--- funciones/email.js
+++ funciones/email.js
@@ -4,7 +4,7 @@
 
 function enviarCorreo() {
     var templateParams = {
-        user_email: 'Fernando.sierra950@outlook.com',
+        user_email: 'fernando.sierra950@outlook.com',
         message: recuentoSummary.textContent,
     };
 
